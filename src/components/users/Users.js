504c3b0bdb8f09522a25f6e7dbdf6ce233f333fe
Augustine Okore <okey.okore@gmail.com>
2021-08-06 20:45:41 +0100

    check in source code

---
+++ src/components/users/Users.js
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react';
+import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+// import PropTypes from 'prop-types'
+import GithubContext from '../../context/github/githubContext';
+
+
+const Users = ()=> {
+    const githubContext = useContext(GithubContext);   
+   const { loading, users } = githubContext;
+   
+    if(loading)
+    {
+        return <Spinner/>
+    }else{
+        return (
+            <div style={userStyle}>
+                {users.map(user=>(
+                    <UserItem key={user.id} user={user} />
+                ))}
+            </div>
+        )
+    }
+       
+}
+ 
+// Users.propTypes={
+//     _users: PropTypes.array.isRequired,
+//     loading: PropTypes.bool.isRequired
+// }
+const userStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem' 
+}
+
+
+export default Users
